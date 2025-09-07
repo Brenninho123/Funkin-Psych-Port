21f683111926d9980b5811cee8d068ff8b82f1be
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-09-07 10:21:56 -0300

    Update DisableSettings.lua

--- scripts/DisableSettings.lua
+++ scripts/DisableSettings.lua
@@ -2,4 +2,5 @@ function onCreatePost()
 setProperty('timeBar.visible','false')
 setProperty('timeBarBG.visible','false')
 setProperty('timeTxt.visible','false')
+setPropertyFromClass('backend.ClientPrefs','data.ghostTapping', false)
 end
